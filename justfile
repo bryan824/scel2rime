e1e9711703a55c6a63d4ef72f63cdc5572477d54
Bryan Tu <bryantu824@gmail.com>
2021-07-14 20:52:19 -0400

    Refactor the break condition
    Why:
    
    How:
    
    Tags:

--- justfile
+++ justfile
@@ -2,3 +2,5 @@ run:
     cargo run
 test:
     cargo run popular_now.scel
+benchmark:
+    hyperfine -m 100 --warmup 3 "$PWD/target/release/scel2rime ./popular_now.scel" "scel2rime ./popular.scel"
--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,23 @@ struct Cli {
     path: PathBuf,
 }
 
+struct WordRecord {
+    word: String,
+    pinyin: String,
+    frequency: u16,
+}
+
+struct Scel {
+    word_list: Vec<WordRecord>,
+    example: String,
+    version: String,
+    id: String,
+    name: String,
+    category: String,
+    scel_length: String,
+    file_name: String,
+}
+
 fn read_string(buffer: &[u8], start: usize, end: usize) -> String {
     let rtn: Vec<u16> = buffer[start..end]
         .chunks_exact(2)
@@ -20,93 +37,147 @@ fn read_string(buffer: &[u8], start: usize, end: usize) -> String {
         .into()
 }
 
-fn main() -> std::io::Result<()> {
-    let path = env::args().nth(1).expect("no path given");
-    let args = Cli {
-        path: PathBuf::from(path),
-    };
-    let buffer = fs::read(&args.path).unwrap();
-    let pre = format!(
-        "# Rime Sogou
-# encoding: utf-8
-#
-# name: {name}
-# type: {category}
-# example: {example}
-#
----
-name: luna_pinyin.sogou.{filename}
-version: \"{version}\"
-sort: by_weight
-use_preset_vocabulary: true
-...
-    ",
-        name = read_string(&buffer, 0x130, 0x338),
-        category = read_string(&buffer, 0x338, 0x540),
-        example = read_string(&buffer, 0xd40, 0x1540),
-        filename = args.path.as_path().file_stem().unwrap().to_string_lossy(),
-        version = SystemTime::now()
-            .duration_since(SystemTime::UNIX_EPOCH)
-            .unwrap()
-            .as_secs()
+fn parse_scel_file(fp: &PathBuf) -> Scel {
+    let buffer = fs::read(fp).unwrap();
+    assert!(
+        &buffer[0x000..=0x000B] == b"\x40\x15\x00\x00\x44\x43\x53\x01\x01\x00\x00\x00",
+        "This is not a scel file of Sogou!"
     );
-    println!("{}", pre);
+
+    let scel_id = read_string(&buffer, 0x001C, 0x011B);
+    let scel_length = u16::from_le_bytes([buffer[0x124], buffer[0x125]]);
+    let name = read_string(&buffer, 0x130, 0x338);
+    let category = read_string(&buffer, 0x338, 0x540);
+    let example = read_string(&buffer, 0xd40, 0x1540);
+    let filename = fp
+        .as_path()
+        .file_stem()
+        .unwrap()
+        .to_string_lossy()
+        .to_string();
+    assert!(
+        &buffer[0x1540..=0x1543] == b"\x9D\x01\x00\x00",
+        "This is not a scel file of Sogou!"
+    );
+
     let mut pos: usize = 0x1544;
     let mut pinyin_lst: Vec<String> = Vec::new();
-    while pos + 4 <= 0x2628 {
+    while u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) == pinyin_lst.len() as u16 {
         let pinyin_len = u16::from_le_bytes([buffer[pos + 2], buffer[pos + 3]]) as usize;
         pos += 4;
         pinyin_lst.push(read_string(&buffer, pos, pos + pinyin_len));
         pos += pinyin_len;
     }
-    let mut pos: usize = 0x2628;
-    let mut word_lst: Vec<String> = Vec::new();
-    let content = 'outer: loop {
-        let word_count = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]);
+
+    assert!(
+        pos == 0x2628,
+        "The starting position of word table is different！！！"
+    );
+    let mut word_list: Vec<WordRecord> = Vec::new();
+    // 一个词条最少需要 22 个字节
+    // word_count: 2
+    // py_count_len: 2
+    // py: 2
+    // word_len: 2
+    // word: 2
+    // extension: 12
+    while pos + 22 <= buffer.len() {
+        let word_count = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) as u8;
         pos += 2;
-        let py_count_len = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]);
+        let py_count_len = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) as usize;
         pos += 2;
-        let mut pinyins: String = String::new();
-        for idx in 0..py_count_len / 2 {
-            let py_idx = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) as usize;
-            if py_idx >= pinyin_lst.len() {
-                break 'outer word_lst.join("\n");
-            }
-            let py = &pinyin_lst[py_idx];
-            if idx != 0 {
-                pinyins.push_str(" ")
-            };
-            pinyins.push_str(&py);
-            pos += 2;
+        if py_count_len % 2 != 0 {
+            println!("Found a word that takes odd number of bytes!!!");
+            break;
         }
+        let pinyin: String = buffer[pos..=pos + py_count_len]
+            .chunks_exact(2)
+            .into_iter()
+            .map(|a| u16::from_le_bytes([a[0], a[1]]))
+            .map(|a| pinyin_lst[a as usize].as_ref())
+            .collect::<Vec<&str>>()
+            .join(" ");
+        pos += py_count_len;
         for _ in 0..word_count {
-            let word_len = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) as usize;
+            if u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) != py_count_len as u16 {
+                println!("The word length is not equal to pinyin length!");
+                break;
+            }
             pos += 2;
-            let word = read_string(&buffer, pos, pos + word_len);
-            pos += word_len;
-            let ext_len = u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) as usize;
-            pos += 2;
-            let w = format!(
-                "{}\t{}\t{}",
-                word,
-                &pinyins,
-                u32::from_le_bytes([
-                    buffer[pos],
-                    buffer[pos + 1],
-                    buffer[pos + 2],
-                    buffer[pos + 3],
-                ])
+            let word = read_string(&buffer, pos, pos + py_count_len);
+            pos += py_count_len;
+            assert!(
+                u16::from_le_bytes([buffer[pos], buffer[pos + 1]]) == 10,
+                "The extension length is not equal to 10 bytes!"
             );
-            pos += ext_len;
-            word_lst.push(w);
+            let freq = u16::from_le_bytes([buffer[pos + 2], buffer[pos + 3]]);
+            word_list.push(WordRecord {
+                word,
+                pinyin: pinyin.clone(),
+                frequency: freq,
+            });
+            pos += 12;
         }
+    }
+    Scel {
+        name,
+        word_list,
+        id: scel_id,
+        scel_length: scel_length.to_string(),
+        example,
+        category,
+        version: SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .unwrap()
+            .as_secs()
+            .to_string(),
+        file_name: filename,
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let path = env::args().nth(1).expect("no path given");
+    let args = Cli {
+        path: PathBuf::from(path),
     };
+    let scel = parse_scel_file(&args.path);
+    let pre = format!(
+        "# Rime Sogou
+# encoding: utf-8
+#
+# 名字: {}
+# ID: {}
+# 类型: {}
+# 例子: {}
+# 词条数目: {}
+---
+name: luna_pinyin.sogou.{}
+version: \"{}\"
+sort: by_weight
+use_preset_vocabulary: true
+...
+    ",
+        scel.name,
+        scel.id,
+        scel.category,
+        scel.example,
+        scel.scel_length,
+        scel.file_name,
+        scel.version
+    );
+    println!("{}", pre);
     fs::write(
+        format!("./luna_pinyin.sogou.{}.dict.yaml", scel.file_name),
         format!(
-            "./luna_pinyin.sogou.{}.dict.yaml",
-            args.path.as_path().file_stem().unwrap().to_string_lossy()
+            "{}\n{}",
+            pre,
+            scel.word_list
+                .iter()
+                .map(|w| format!("{}\t{}\t{}", w.word, w.pinyin, w.frequency))
+                .collect::<Vec<String>>()
+                .join("\n"),
         ),
-        format!("{}\n{}", pre, content),
     )?;
+    println!("{} word records are loaded!!!", scel.scel_length);
     Ok(())
 }
